619ad2c0483716161ae599aab4e0cf5b4ed724d5
Darshkhandelwal <101493985+Darshkhandelwal@users.noreply.github.com>
2022-05-14 18:52:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function preload(){
+seaImg = loadimage("sea.png");
+shipImg1 = loadAnimation();
+spriteName.addImage(seaImg);
+}
+
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+ 
+}
